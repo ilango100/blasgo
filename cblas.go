bb5c3e3d88c77cc0832fee98e512087c6634cfdf
Ilango R <ilangokul@gmail.com>
2019-02-20 01:59:21 +0530

    SSWAP DSWAP done

--- cblas.go
+++ cblas.go
@@ -2,19 +2,26 @@ package blasgo
 
 /*
 #include "cblas.h"
+typedef CBLAS_INDEX BlasInd;
 */
 import "C"
 
-import (
-	"unsafe"
-)
-
 //SNRM2 calculates Euclidean norm vector
 func SNRM2(N int, X []float32, incX int) float32 {
-	return float32(C.cblas_snrm2(C.int(N), (*C.float)(unsafe.Pointer(&X[0])), C.int(incX)))
+	return float32(C.cblas_snrm2(C.int(N), (*C.float)(&X[0]), C.int(incX)))
 }
 
 //DNRM2 calculates Euclidean norm vector
 func DNRM2(N int, X []float64, incX int) float64 {
-	return float64(C.cblas_dnrm2(C.int(N), (*C.double)(unsafe.Pointer(&X[0])), C.int(incX)))
+	return float64(C.cblas_dnrm2(C.int(N), (*C.double)(&X[0]), C.int(incX)))
+}
+
+//SSWAP swaps the Vectors
+func SSWAP(N int, X []float32, incX int, Y []float32, incY int) {
+	C.cblas_sswap(C.int(N), (*C.float)(&X[0]), C.int(incX), (*C.float)(&Y[0]), C.int(incY))
+}
+
+//DSWAP swaps the Vectors
+func DSWAP(N int, X []float64, incX int, Y []float64, incY int) {
+	C.cblas_dswap(C.int(N), (*C.double)(&X[0]), C.int(incX), (*C.double)(&Y[0]), C.int(incY))
 }
--- cblas_test.go
+++ cblas_test.go
@@ -1,14 +1,44 @@
 package blasgo
 
-import "testing"
+import (
+	"testing"
+)
+
+const n = 10
 
 var a = []float32{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+var b = []float32{10, 9, 8, 7, 6, 5, 4, 3, 2, 1}
 var aa = []float64{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+var bb = []float64{10, 9, 8, 7, 6, 5, 4, 3, 2, 1}
 
 const anorm = 19.621416870348583468526003789181774326312834524158
 
+func compareSlices32(a, b []float32) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	for i := range a {
+		if a[i] != b[i] {
+			return false
+		}
+	}
+	return true
+}
+
+func compareSlices64(a, b []float64) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	for i := range a {
+		if a[i] != b[i] {
+			return false
+		}
+	}
+	return true
+}
+
 func TestSNRM2(t *testing.T) {
-	nrm := SNRM2(len(a), a, 1)
+	nrm := SNRM2(n, a, 1)
 	t.Logf("Norm of %v is %f", a, nrm)
 	if nrm != anorm {
 		t.Errorf("Norm wanted %f, got: %f", anorm, nrm)
@@ -16,9 +46,31 @@ func TestSNRM2(t *testing.T) {
 }
 
 func TestDNRM2(t *testing.T) {
-	nrm := DNRM2(len(aa), aa, 1)
+	nrm := DNRM2(n, aa, 1)
 	t.Logf("Norm of %v is %f", a, nrm)
 	if nrm != anorm {
 		t.Errorf("Norm wanted %.20f, got: %.20f", anorm, nrm)
+	}
+}
+
+func TestSSWAP(t *testing.T) {
+	aaa := make([]float32, n)
+	copy(aaa, a)
+	bbb := make([]float32, n)
+	copy(bbb, b)
+	SSWAP(n, aaa, 1, bbb, 1)
+	if !compareSlices32(aaa, b) || !compareSlices32(bbb, a) {
+		t.Errorf("Not swapped correctly, a: %v, b: %v", aaa, bbb)
+	}
+}
+
+func TestDSWAP(t *testing.T) {
+	aaa := make([]float64, n)
+	copy(aaa, aa)
+	bbb := make([]float64, n)
+	copy(bbb, bb)
+	DSWAP(n, aaa, 1, bbb, 1)
+	if !compareSlices64(aaa, bb) || !compareSlices64(bbb, aa) {
+		t.Errorf("Not swapped correctly, a: %v, b: %v", aaa, bbb)
 	}
 }
