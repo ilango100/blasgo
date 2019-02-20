d8c4ad83a1c00a1ab5b1ba279588b993fa03ae4c
Ilango R <ilangokul@gmail.com>
2019-02-21 00:34:29 +0530

    complex.h not needed

--- lv1basic.go
+++ lv1basic.go
@@ -1,7 +1,6 @@
 package blasgo
 
 /*
-#include <complex.h>
 #include "cblas.h"
 */
 import "C"
@@ -10,45 +9,45 @@ import (
 	"unsafe"
 )
 
-//Idx indicates index of vector or matrix
+//Idx indicates index of vector or matrix.
 type Idx C.CBLAS_INDEX
 
-//SNRM2 calculates Euclidean norm of vector
+//SNRM2 calculates Euclidean norm of the vector.
 func SNRM2(N int, X []float32, incX int) float32 {
 	return float32(C.cblas_snrm2(C.int(N), (*C.float)(&X[0]), C.int(incX)))
 }
 
-//DNRM2 calculates Euclidean norm of vector
+//DNRM2 calculates Euclidean norm of the vector.
 func DNRM2(N int, X []float64, incX int) float64 {
 	return float64(C.cblas_dnrm2(C.int(N), (*C.double)(&X[0]), C.int(incX)))
 }
 
-//SCNRM2 calculates Euclidean norm of vector
+//SCNRM2 calculates Euclidean norm of the vector.
 func SCNRM2(N int, X []complex64, incX int) float32 {
 	return float32(C.cblas_scnrm2(C.int(N), unsafe.Pointer(&X[0]), C.int(incX)))
 }
 
-//DZNRM2 calculates Euclidean norm of vector
+//DZNRM2 calculates Euclidean norm of the vector.
 func DZNRM2(N int, X []complex128, incX int) float64 {
 	return float64(C.cblas_dznrm2(C.int(N), unsafe.Pointer(&X[0]), C.int(incX)))
 }
 
-//SSWAP swaps the Vectors
+//SSWAP swaps the Vectors.
 func SSWAP(N int, X []float32, incX int, Y []float32, incY int) {
 	C.cblas_sswap(C.int(N), (*C.float)(&X[0]), C.int(incX), (*C.float)(&Y[0]), C.int(incY))
 }
 
-//DSWAP swaps the Vectors
+//DSWAP swaps the Vectors.
 func DSWAP(N int, X []float64, incX int, Y []float64, incY int) {
 	C.cblas_dswap(C.int(N), (*C.double)(&X[0]), C.int(incX), (*C.double)(&Y[0]), C.int(incY))
 }
 
-//SCOPY copies the Vectors
+//SCOPY copies the Vector X into Y.
 func SCOPY(N int, X []float32, incX int, Y []float32, incY int) {
 	C.cblas_scopy(C.int(N), (*C.float)(&X[0]), C.int(incX), (*C.float)(&Y[0]), C.int(incY))
 }
 
-//DCOPY copies the Vectors
+//DCOPY copies the Vector X into Y.
 func DCOPY(N int, X []float64, incX int, Y []float64, incY int) {
 	C.cblas_dcopy(C.int(N), (*C.double)(&X[0]), C.int(incX), (*C.double)(&Y[0]), C.int(incY))
 }
